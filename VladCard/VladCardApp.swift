fd7bb7489cc8cc1b07a98dfa119b753e98868eb1
VladVorniceanu <vorniceanu.v@gmail.com>
2024-03-05 11:09:30 +0200

    Initial Commit

---
+++ VladCard/VladCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VladCardApp.swift
+//  VladCard
+//
+//  Created by Vlad Vorniceanu on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VladCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
